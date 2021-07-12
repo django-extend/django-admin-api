49a056db247b82ba3b302acd5de7e22f7e6fa9d2
nick <you@example.com>
2021-07-12 16:50:44 +0800

    update

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
 setup(
     name='django-admin-api',
     version='0.0.1',
@@ -7,6 +11,8 @@ setup(
     include_package_data=True,
     license='BSD License',  # example license
     description='export Django Admin API by restful style',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     url='https://github.com/django-extend/django-admin-api.git',
     author='Nieo Ding',
     author_email='8285770@qq.com',
@@ -18,11 +24,22 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Development Status :: 3 - Alpha'
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
